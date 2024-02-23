93148fcddcc1ed4cabfd07088f7ad2641c60f2a8
Yasmeen Othman <yasmeen.othman20@gmail.com>
2024-02-23 20:33:13 +0200

    react quiz

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Questions from "./components/Questions";
+
+function App() {
+  return (
+    <div className="App">
+      <Questions />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Questions.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import questions from "./QuestionsBank";
+import "./Question.css";
+
+function Questions() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+  return (
+    <div className="app">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+
+              {questions[currentQuestion].questionCode ? (
+                <div className="question-code">
+                  <span>{questions[currentQuestion].questionCode}</span>{" "}
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button
+                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
+              >
+                {answerOption.answerText}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Questions;
